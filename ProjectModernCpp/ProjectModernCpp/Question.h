f29d50a0171fe8e344f4a59a8f0c8ebd3c5732af
bogdan_deaconu <bogdan.deaconu02@gmail.com>
2022-11-11 17:37:33 +0200

    ReMake  Question Class to Create a Base Class for QuizBool and QuizInteger

--- ProjectModernCpp/ProjectModernCpp/Question.h
+++ ProjectModernCpp/ProjectModernCpp/Question.h
@@ -6,19 +6,12 @@
 class Question
 {
 public:
-	Question(std::string question, std::unordered_map<std::string, bool> answers);
+	Question(std::string question);
 	~Question();
 
 	const std::string getQuestion() const;
-	const std::unordered_map<std::string,bool> getAnswers() const;
-	
-	bool CheckAnswer(std::string answer,const Question& question);
-
-	friend std::ostream& operator<<(std::ostream& os, const Question& question);
-	
-	
 private:
 	std::string m_question;
-	std::unordered_map<std::string, bool>m_answers;
+	
 };
 
