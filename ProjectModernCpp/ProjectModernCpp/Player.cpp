e11c0b919dcbca8a0f85dd552b6276caaf10508a
FitaRobert <fita.robert@yahoo.com>
2022-12-04 21:36:57 +0200

    Set and get score for players

--- ProjectModernCpp/ProjectModernCpp/Player.cpp
+++ ProjectModernCpp/ProjectModernCpp/Player.cpp
@@ -2,29 +2,34 @@
 #include "Territory.h"
 
 
-/*Player& Player::operator=(Player&& player)
+Player& Player::operator=(const Player& player)
 {
 	m_color = player.m_color;
-	new (&player) Player;
 	return *this;
 }
-*/
-
 
 std::ostream& operator<<(std::ostream& os, const Player& player)
 {
 	return os <<
 		static_cast<std::string>(player.m_name) << " " <<
-		static_cast<int>(player.m_color);
+		static_cast<int>(player.m_color) << " " <<
+		static_cast<uint32_t>(player.m_score) << std::endl;
 }
 
-Player::Player(const std::string& name, Color color) :
+Player::Player(const std::string& name, Color color, uint32_t score, Status status) :
 	m_name(name),
-	m_color(color)
+	m_color(color),
+	m_score(score),
+	m_status(status)
 {
+
 }
 
-Player::Player()
+Player::Player() :
+	m_name(""),
+	m_color(),
+	m_score(),
+	m_status()
 {
 }
 
@@ -33,23 +38,14 @@ Player::Color Player::GetColor() const
 	return m_color;
 }
 
-void Player::setBase(Board::Position pos, Territory& base, const Player& player)
-{
-	base.ConquerTerritory(player.GetColor());
-	base.SetTerritoryScore(300);
-	board[pos] = base;
-}
-
-void Player::setInactivePlayer(Player player, Territory base)
-{
-	/*if (base.GetColor() != player.GetColor())
-		player.m_name = "Inactiv";
-*/
-}
-
 void Player::SetName(const std::string& name)
 {
 	m_name = name;
+}
+
+std::string Player::GetName() const
+{
+	return m_name;
 }
 
 void Player::SetColor(const Player::Color& color)
@@ -57,11 +53,48 @@ void Player::SetColor(const Player::Color& color)
 	m_color = color;
 }
 
+void Player::SetScore(const uint32_t& score)
+{
+	m_score = score;
+}
+
+uint32_t Player::GetScore() const
+{
+	return m_score;
+}
+
+void Player::SetStatus(const Player::Status& status)
+{
+	m_status = status;
+}
+
+Player::Status Player::GetStatus() const
+{
+	return m_status;
+}
+
 int Player::PlayerAnswer()
 {
 	int answer;
 	std::cin >> answer;
 	return answer;
+}
+
+void Player::WinDuel()
+{
+	m_score = m_score + 100;
+}
+
+void Player::LoseDuel()
+{
+	m_score = m_score - 100;
+}
+
+void Player::setBase(Board::Position pos, Territory& base, const Player& player)
+{
+	base.ConquerTerritory(player.GetColor());
+	base.SetTerritoryScore(300);
+	//board[pos] = base;
 }
 
 
