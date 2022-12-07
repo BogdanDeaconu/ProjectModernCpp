e40910b5c86a77531c541b57af56f5652d8a9b17
FitaRobert <fita.robert@yahoo.com>
2022-12-07 16:14:52 +0200

    Repair Player.h,Player.cpp and Game class

--- ProjectModernCpp/ProjectModernCpp/Player.h
+++ ProjectModernCpp/ProjectModernCpp/Player.h
@@ -14,27 +14,37 @@ public:
 		Red,
 		Yellow,
 		Green
-	}; 
-		
- 
+	};
 public:
-	Player(const std::string& name, Color color);
+	enum class Status :uint8_t
+	{
+		Inactiv,
+		Activ
+	};
+
+public:
+	Player(const std::string& name, Color color, uint32_t score, Status status);
 	Player();
-	Player& operator=(Player&& player);
-	Color GetColor() const;
-	
+	Player& operator=(const Player& player);
 	friend std::ostream& operator <<(std::ostream& os, const Player& player);
-	void setBase(Board::Position pos, Territory& base, const Player& player);
-	void setInactivePlayer(Player player, Territory base);
 	void SetName(const std::string& name);
+	std::string GetName() const;
 	void SetColor(const Player::Color& color);
-	
+	Color GetColor() const;
+	void SetScore(const uint32_t& score);
+	uint32_t GetScore() const;
+	void SetStatus(const Player::Status& status);
+	Status GetStatus() const;
 	int PlayerAnswer();
-	
+	void WinDuel();
+	void LoseDuel();
+
 private:
 	std::string m_name;
 	Color m_color;
-	Board board;
+	uint32_t m_score;
+	Status m_status;
 
 };
+
 
