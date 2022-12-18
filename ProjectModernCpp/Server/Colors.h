6fd96976b84ec47ef8361b43b91391aad23f1b47
FitaRobert <fita.robert@yahoo.com>
2022-12-18 23:16:18 +0200

    Colors and Player class to server.

---
+++ ProjectModernCpp/Server/Colors.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+enum class Color : uint8_t
+{
+	Blue,
+	Red,
+	Yellow,
+	Green,
+	Grey
+};
+
---
+++ ProjectModernCpp/Server/Player.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Colors.h"
+
+class Player
+{
+
+public:
+	enum class Status :uint8_t
+	{
+		Inactiv,
+		Activ
+	};
+
+public:
+	Player(const std::string& name, Color color, uint32_t score, Status status);
+	Player();
+	Player& operator=(const Player& player);
+	friend std::ostream& operator <<(std::ostream& os, const Player& player);
+	void SetName(const std::string& name);
+	std::string GetName() const;
+	void SetColor(const Color& color);
+	Color GetColor() const;
+	void SetScore(const uint32_t& score);
+	uint32_t GetScore() const;
+	void SetStatus(const Player::Status& status);
+	Status GetStatus() const;
+	int PlayerAnswer();
+	void WinDuel();
+	void LoseDuel();
+
+private:
+	std::string m_name;
+	Color m_color;
+	uint32_t m_score;
+	Status m_status;
+
+};
+
+
