50661a3b1338924490cfe57e6807d1f53bacdfb2
bogdan_deaconu <bogdan.deaconu02@gmail.com>
2022-12-20 04:04:21 +0200

    End Enter and Star Game and Add Wins and Losses in UserMeniu

--- ProjectModernCpp/Client/Meniu.cpp
+++ ProjectModernCpp/Client/Meniu.cpp
@@ -12,9 +12,9 @@ Meniu::~Meniu()
 void Meniu::on_StartGameButton_clicked()
 {
 	cpr::Response r = cpr::Get(cpr::Url{ "http://localhost:18080/playerstartgame" });
-	auto numberofplayer = crow::json::load(r.text);
-	
-	if (numberofplayer["numberofplayer"] == 1) {
+	auto load = crow::json::load(r.text);
+	int numberofplayers = load["numberofplayers"].i();
+	if (numberofplayers == 1) {
 		QMessageBox::information(this, "Error", "Game started, Wait for players");
 	}
 	else {
@@ -26,15 +26,19 @@ void Meniu::SetUserMeniu(QString username)
 {
 	//ui.HomePage->setText(qstringUser);
 	ui.Account->setText(username);
+	ui.Wins->setText("Wins: 0");
+	ui.Wins->setFont(QFont("Times", 12, QFont::Bold, false));
+	ui.lineEdit_5->setText("Losses: 0");
+	ui.lineEdit_5->setFont(QFont("Times", 12, QFont::Bold, false));
 	ui.Account->setFont(QFont("Times New Roman", 20, QFont::Bold, false));
 }
 
 void Meniu::on_EnterGameButton_clicked()
 {
 	cpr::Response r = cpr::Get(cpr::Url{ "http://localhost:18080/playerjoingame" });
-	auto numberofplayer = crow::json::load(r.text);
-
-	if (numberofplayer["numberofplayer"] == 2) {
+	auto load = crow::json::load(r.text);
+	int nuberofplayer = load["numberofplayers"].i();
+	if (nuberofplayer == 2) {
 		QMessageBox::information(this, "Error", "Game started, Wait for players");
 	}
 	else {
