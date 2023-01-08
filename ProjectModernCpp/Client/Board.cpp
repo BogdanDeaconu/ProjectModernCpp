aab5df5c10c0b1040743cdb4ad259aa8ad277390
bogdan_deaconu <bogdan.deaconu02@gmail.com>
2023-01-08 23:04:44 +0200

    Implement Button in Cpp for Board

--- ProjectModernCpp/Client/Board.cpp
+++ ProjectModernCpp/Client/Board.cpp
@@ -8,3 +8,47 @@ Board::Board(QWidget *parent)
 
 Board::~Board()
 {}
+
+void Board::on_PushButton2_clicked()
+{
+}
+
+void Board::on_PushButton3_clicked()
+{
+}
+
+void Board::on_PushButton4_clicked()
+{
+}
+
+void Board::on_PushButton5_clicked()
+{
+}
+
+void Board::on_PushButton6_clicked()
+{
+}
+
+void Board::on_PushButton7_clicked()
+{
+}
+
+void Board::on_PushButton8_clicked()
+{
+}
+
+void Board::on_PushButton9_clicked()
+{
+}
+
+void Board::on_PushButton10_clicked()
+{
+}
+
+void Board::on_PushButton11_clicked()
+{
+}
+
+void Board::on_PushButton12_clicked()
+{
+}
